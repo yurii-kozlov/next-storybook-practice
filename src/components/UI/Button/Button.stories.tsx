9231049b02eaec00568a1003341c7f7886c0276a
Yurii Kozlov <yurka22870@gmail.com>
2024-10-07 22:17:59 +0300

    feat: add renaming and sorting stories

--- src/components/UI/Button/Button.stories.tsx
+++ src/components/UI/Button/Button.stories.tsx
@@ -11,23 +11,27 @@ const Template: StoryFn<typeof Button> = (args) => <Button {...args}/>
 export const Primary = Template.bind({});
 Primary.args = {
   variant: 'primary',
-  children: 'Primary'
+  children: 'Primary',
 };
+Primary.storyName = 'Primary button'
 
 export const Secondary = Template.bind({});
 Secondary.args = {
   variant: 'secondary',
   children: 'Secondary'
 };
+Secondary.storyName = 'Secondary button'
 
 export const Danger = Template.bind({});
 Danger.args = {
   variant: 'danger',
   children: 'Danger'
 };
+Danger.storyName = 'Danger button'
 
 export const Success = Template.bind({});
 Success.args = {
   variant: 'success',
   children: 'Success'
 }
+Success.storyName = 'Success button'
