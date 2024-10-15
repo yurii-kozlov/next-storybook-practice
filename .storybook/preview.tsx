4c4f2b2580acd4c9f7ddead59b451c31e7d317e0
Yurii Kozlov <yurka22870@gmail.com>
2024-10-15 16:44:37 +0300

    fix: fix eof

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,14 +1,14 @@
 import type { Preview } from "@storybook/react";
-import { withCenter } from './decorators/withCenter';
-import { withThemeProvider } from './decorators/withThemeProvider';
+import { withCenter } from "./decorators/withCenter";
+import { withThemeProvider } from "./decorators/withThemeProvider";
 import { withConsole } from "./decorators/withConsole";
-import '@storybook/addon-console';
-import { withKnobs } from '@storybook/addon-knobs';
+import "@storybook/addon-console";
+import { withKnobs } from "@storybook/addon-knobs";
 
 const preview: Preview = {
   parameters: {
     docs: {
-      toc: true
+      toc: true,
     },
     controls: {
       matchers: {
@@ -18,8 +18,7 @@ const preview: Preview = {
     },
     options: {
       // The `a` and `b` arguments in this function have a type of `import('@storybook/types').IndexEntry`. Remember that the function is executed in a JavaScript environment, so use JSDoc for IntelliSense to introspect it.
-      storySort: (a, b) =>
-        a.id === b.id ? 0 : a.id.localeCompare(b.id, undefined, { numeric: true }),
+      storySort: (a, b) => (a.id === b.id ? 0 : a.id.localeCompare(b.id, undefined, { numeric: true })),
     },
   },
   decorators: [withThemeProvider, withCenter, withConsole, withKnobs],
